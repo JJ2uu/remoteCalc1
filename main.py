bdaa7f01e694dd084007be42d07ea8b22d77429e
JJuu <jyj120572@gmail.com>
2023-02-28 11:42:40 +0900

    Master 1

---
+++ main.py
@@ -0,0 +1,14 @@
+from add import add_func
+from min import min-func
+
+## 함수
+
+## 전역
+num1, num2, result = 100, 200, 0
+
+## 메인
+result = add_func(num1, num2)
+print(result)
+
+result = min_func(num1, num2)
+print(result)
